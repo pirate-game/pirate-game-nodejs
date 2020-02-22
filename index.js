e51abc13711056ec04434dc46b91af81219b4a8b
pirate-game <60094408+pirate-game@users.noreply.github.com>
2020-02-22 15:43:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-/*io.on('connection', function(socket){
+io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
@@ -16,4 +16,3 @@ app.get('/', function(req, res){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-*/
