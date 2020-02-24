2a055a76e35c370681fd651535eed2adb8c1dfc1
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-02-24 18:41:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ app.get('/', function(req, res){
 app.use(express.static('public'));
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('pirate game', function(msg){
+    io.emit('pirate game', msg);
   });
 });
 
-http.listen(port, function(){
+/*http.listen(port, function(){
   console.log('listening on *:' + port);
-});
+});*/
+//This may be needed?
 
 app.use(function (req, res, next) {
   res.status(404).sendFile(__dirname + '/404.html');
