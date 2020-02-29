ca839ec21ae6a4c3264f44ddb21e96d8d2ed297a
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-02-29 12:09:15 +0000

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -1,11 +1,11 @@
 const navbar = <div>
 	    		<ul>
 				<li><a id="logo" href="index.html"><img border="0" src="logo.png" /></a></li>
-				<li><a href="Start.html">Start&nbspa<br />Game</a></li>
-				<li><a href="Join.html">Join&nbspa<br />Game</a></li>
-				<li><a href="Watch.html">Watch&nbspa<br />Game</a></li>
+				<li><a href="Start.html">Start<Nbsp />a<br />Game</a></li>
+				<li><a href="Join.html">Join<Nbsp />a<br />Game</a></li>
+				<li><a href="Watch.html">Watch<Nbsp />a<br />Game</a></li>
 				<li><a href="Acknowledge.html">Acknowledgements</a></li>
 	    		</ul>
-	    		<h1 id="title"><u>&nbspThe&nbspPirate&nbspGame&nbsp</u></h1>
+	    		<h1 id="title"><u><Nbsp />The<Nbsp />Pirate<Nbsp />Game<Nbsp /></u></h1>
 		</div>;
 ReactDOM.render(navbar, document.getElementById("nav"));
