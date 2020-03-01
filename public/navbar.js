c3a37ec9931be9a14e3ff9e369812f91f9d19543
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-01 11:55:34 +0000

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -2,9 +2,9 @@ const navbar = <div>
       			<link rel="stylesheet" type="text/css" href="css/navbar.css" />
 	    		<ul>
 				<li><a id="logo" href="index.html"><img border="0" src="logo.png" /></a></li>
-				<li><a href="Start.html">Start<Nbsp />a<br />Game</a></li>
-				<li><a href="Join.html">Join<Nbsp />a<br />Game</a></li>
-				<li><a href="Watch.html">Watch<Nbsp />a<br />Game</a></li>
+				<li><a href="Start.html">Start a<br />Game</a></li>
+				<li><a href="Join.html">Join a<br />Game</a></li>
+				<li><a href="Watch.html">Watch a<br />Game</a></li>
 				<li><a href="Acknowledge.html">Acknowledgements</a></li>
 	    		</ul>
 	    		<h1 id="title">The Pirate Game</h1>
