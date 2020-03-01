bb5f85615a24b171c10726ec70b97dd835529b54
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-01 11:53:14 +0000

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -1,7 +1,3 @@
-function Nbsp() {
-  return '\u00A0';
-}
-
 const navbar = <div>
       			<link rel="stylesheet" type="text/css" href="css/navbar.css" />
 	    		<ul>
@@ -11,6 +7,6 @@ const navbar = <div>
 				<li><a href="Watch.html">Watch<Nbsp />a<br />Game</a></li>
 				<li><a href="Acknowledge.html">Acknowledgements</a></li>
 	    		</ul>
-	    		<h1 id="title"><Nbsp />The<Nbsp />Pirate<Nbsp />Game<Nbsp /></h1>
+	    		<h1 id="title">The Pirate Game</h1>
 		</div>;
 ReactDOM.render(navbar, document.getElementById("nav"));
