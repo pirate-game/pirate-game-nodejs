9d68d5062e7bfcbf4e864bebbfb08a30405406c7
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-08 14:17:12 +0000

    Update Start.js

--- public/Start.js
+++ public/Start.js
@@ -5,24 +5,24 @@ var key = '';
 class KeyBox extends React.Component {
   constructor() {
     super();
-    this.state = {visibilty: "hidden"};
+    this.state = {visibilty: 'hidden'};
   }
   render() {
     return <h2 style={{visibilty: this.state.visibility}}> Key: {key} </h2>;
   }
   show(){
-    this.state.visibility = "visible";
+    this.state.visibility = 'visible';
   }
   hide(){
-    this.state.visibility = "hidden";
+    this.state.visibility = 'hidden';
 };
 
 socket.on('key', function(msg){
   key = msg;
-  document.getElementById("KeyBox").show();
+  document.getElementById('KeyBox').show();
 });
 
 socket.emit('request_key', '');
 
-var element = <KeyBox id="KeyBox"/>;
+var element = <KeyBox id='KeyBox' />;
 ReactDOM.render(element, root);
