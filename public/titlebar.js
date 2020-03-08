50224884588486458ae84376aabb4fcc04039616
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-08 19:00:37 +0000

    Update titlebar.js

--- public/titlebar.js
+++ public/titlebar.js
@@ -1,5 +1,5 @@
 const titlebar = <div>
       			<link rel="stylesheet" type="text/css" href="css/titlebar.css" />
-	    		  <h1 id="title">The&nbsp;Pirate&nbsp;Game</h1>
+	    		  <h1 id="title" style={{fontSize: '50px'}}>The&nbsp;Pirate&nbsp;Game</h1>
 		</div>;
 ReactDOM.render(titlebar, document.getElementById("titlebar"));
