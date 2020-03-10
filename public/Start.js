9314e69c7494567f1ec8328ae9d4758b11d071bb
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-10 19:12:14 +0000

    Update Start.js

--- public/Start.js
+++ public/Start.js
@@ -22,7 +22,7 @@ class CrewUl extends React.Component {
     super();
     this.state = {crew: []};
     socket.on('request_join', function(name){
-      this.setState({crew:this.state.crew+[name]);
+      this.setState({crew:this.state.crew+[name]});
     });
   }
   render() {
