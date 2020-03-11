b0190cff862aef09e0f29e32073c26b7ec235315
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-11 17:41:24 +0000

    Update Start.js

--- public/Start.js
+++ public/Start.js
@@ -26,6 +26,7 @@ class CrewUl extends React.Component {
     });
   }
   render() {
+    console.log(this.state.crew);
     return <div id="crewDiv">
       <ul id="crewUl">
           {this.state.crew.map(crewMember => (
