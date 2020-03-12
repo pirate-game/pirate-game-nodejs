7bf2c4c28219a7e9b9816ff10f28c3e7d85086df
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-12 18:35:28 +0000

    Update PopUp.js

--- public/PopUp.js
+++ public/PopUp.js
@@ -1,5 +1,5 @@
-class PopUp extends React.Component {
+export class PopUp extends React.Component {
   render() {
     return <div id={this.props.id} className={this.props.className+" popUp"} style={this.props.style}>{this.props.children}</div>;
   }
-};
+}
