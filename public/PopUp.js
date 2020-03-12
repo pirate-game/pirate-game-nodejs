b151df95b8cd0ab669d2504bde82cba6e5a0d1ed
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-12 18:40:10 +0000

    Delete PopUp.js

--- public/PopUp.js
+++
@@ -1,5 +0,0 @@
-export class PopUp extends React.Component {
-  render() {
-    return <div id={this.props.id} className={this.props.className+" popUp"} style={this.props.style}>{this.props.children}</div>;
-  }
-}
