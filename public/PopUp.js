9d27ff30a9077e7cd8f71b2a648965f19a7bf01f
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-12 18:23:24 +0000

    Update PopUp.js

--- public/PopUp.js
+++ public/PopUp.js
@@ -1,10 +1,5 @@
 class PopUp extends React.Component {
-  const popUpStyle = {
-    
-  };
   render() {
-    var X = this.props.style;
-    Object.assign(X, popUpStyle);
-    return <div id={this.props.id} className={this.props.className} style={X}>{this.props.children}</div>;
+    return <div id={this.props.id} className={this.props.className+" popUp"} style={this.props.style}>{this.props.children}</div>;
   }
 };
