9a9c6893dceef1cdec6bea56642b44c0026de279
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-20 15:36:07 +0000

    Update Join.js

--- public/Join.js
+++ public/Join.js
@@ -1,7 +1,8 @@
 var root = document.getElementById('root');
 var socket = io();
 
-const namePattern = /^[\w| ]*$/;
+const namePattern = /^[\w'-" ]*$/;
+const exclPattern = /^\s*$/;
 const keyPattern = /^[0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef][0-9abcdef]$/;
 
 function hidePopUps(){
@@ -22,10 +23,16 @@ socket.on('name_taken', function(){
   document.getElementById("nameTaken").style.display = "block";  
 });
 
+socket.on('game_unavailable', function(){
+  document.getElementById("gameKey").value = "";
+  hidePopUps();
+  document.getElementById("gameUnavailable").style.display = "block";  
+});
+
 function attemptJoin(){
   var name = document.getElementById("pirateName").value;
   var key = document.getElementById("gameKey").value;
-  if (namePattern.test(name)) {
+  if (namePattern.test(name) && !exclPattern.test(name)) {
     if (keyPattern.test(key)) {
       hidePopUps();
       document.getElementById("waiting").style.display = "block";
@@ -53,13 +60,46 @@ var toRender = <div>
   <button id="join" onClick={attemptJoin}>Join</button>
 </div>
 <div id="popUps">
-  <div id="invalidName" className="popUp"><div><p>invalidName</p></div></div>
-  <div id="invalidKey" className="popUp"><div><p>invalidKey</p></div></div>
-  <div id="waiting" className="popUp"><div><p>waiting</p></div></div>
-  <div id="noSuchGame" className="popUp"><div><p>noSuchGame</p></div></div>
-  <div id="nameTaken" className="popUp"><div><p>nameTaken</p></div></div>
+      
+  <div id="invalidName" className="popUp"><div>
+      <h3>Invalid Name</h3>
+      <p>Please choose a name consisting of alphanumeric characters and spaces but which is not entirely whitespace and especially isn&apos;t the empty name.</p>
+      <button className="close" onClick={hidePopUps}>Okay!</button>
+  </div></div>
+      
+  <div id="invalidKey" className="popUp"><div>
+      <h3>Invalid Key</h3>
+      <p>Please correctly type the key you have been given. It should be 6 hexadecimal digits e.g. &quot;beef54&quot;.
+        <br />Please use all lower-case letters.</p>
+      <button className="close" onClick={hidePopUps}>Okay!</button>
+  </div></div>
+      
+  <div id="waiting" className="popUp"><div>
+      <h3>Waiting</h3>
+      <p>This usually means that the game is about to start. :) 
+        <br />However, sometimes it means that something has gone wrong and you may need to try to join again. :(</p>
+  </div></div>
+  
+  <div id="noSuchGame" className="popUp"><div>
+      <h3>No Such Game</h3>
+      <p>You&apos;ve probably mistyped your key. Try again.</p>
+      <button className="close" onClick={hidePopUps}>Okay!</button>                                                                     
+  </div></div>
+                                                                                                                 
+  <div id="nameTaken" className="popUp"><div>
+      <h3>Name Taken</h3>
+      <p>Great minds think alike! (and fools&apos; seldom differ)
+        <br /> Someone&apos;s taken your name already. Choose another one.</p>
+      <button className="close" onClick={hidePopUps}>Okay!</button>
+  </div></div>
+    
+  <div id="gameUnavailable" className="popUp"><div>
+    <h3>Game Unavailable</h3>
+      <p>Unfortunately for you, this probably means they&apos;ve started without you. :(</p>
+      <button className="close" onClick={hidePopUps}>Okay!</button>
+  </div></div>
 </div>
 </div>;
 
 ReactDOM.render(toRender, root);
-var popUps = document.getElementsByClassName("popUp");
+var popUps = document.getElementsByClassName("popUp");  
