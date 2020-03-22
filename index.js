75c252aad489210e6d792c3e3004e9b39beef12b
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-22 15:45:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ io.on('connection', function(socket){
   });
   
   socket.on('crew_assembled', function(){
+    socket.emit('debugmsg', games);
     socket.emit('debugmsg', 'First');
     var pos = leaderToGame(socket);
     socket.emit('debugmsg', 'Second');
@@ -93,9 +94,11 @@ io.on('connection', function(socket){
       socket.emit('show_provisional_crew');
       socket.emit('debugmsg', 'Fifth');
     };
+    socket.emit('debugmsg', games);
   });
   
   socket.on('remove_player', function(who){
+    socket.emit('debugmsg', games);
     socket.emit('debugmsg', 'First');
     var pos = leaderToGame(socket);
     socket.emit('debugmsg', pos);
@@ -106,6 +109,7 @@ io.on('connection', function(socket){
         player.emit('join_rejected')
       };
     };
+    socket.emit('debugmsg', games);
   });
   
 });
