5aa173d16308b4e5c679f833ae26e89965f59aae
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-23 16:16:43 +0000

    Update Start.js

--- public/Start.js
+++ public/Start.js
@@ -7,21 +7,21 @@ socket.on('debugmsg', function(msg){console.log(msg)});
 
 function showStage(stage){
   var stageElements = document.getElementsByClassName(stage);
-  for (var i; i < stageElements.length; i++){
+  for (var i = 0; i < stageElements.length; i++){
     stageElements[i].style.display = "block";
   };
 };
 
 function hideStage(stage){
   var stageElements = document.getElementsByClassName(stage);
-  for (var i; i < stageElements.length; i++){
+  for (var i = 0; i < stageElements.length; i++){
     stageElements[i].style.display = "none";
   };
 };
 
 function hidePopUps(){
   var popUps = document.getElementsByClassName("popUp");
-  for (var i; i < popUps.length; i++){
+  for (var i = 0; i < popUps.length; i++){
     popUps[i].style.display = "none";
   };
 };
