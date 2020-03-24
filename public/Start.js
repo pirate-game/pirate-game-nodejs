4498b125ba5d018140be4e0e3e85b58f7bd696b8
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-24 11:22:58 +0000

    Update Start.js

--- public/Start.js
+++ public/Start.js
@@ -26,12 +26,6 @@ function hidePopUps(){
   };
 };
 
-/*function hidePopUps(){
-  for (var t = 0; t < popUps.length; t++){
-    popUps[t].style.display = "none";
-  };
-};*/
-
 class KeyBox extends React.Component {
   constructor() {
     super();
@@ -49,6 +43,7 @@ class CrewUl extends React.Component {
   constructor() {
     super();
     this.state = {crew: []};
+    
     socket.on('request_join', name => {
       this.setState({crew:this.state.crew.concat([name])});
       globalCrew.push(name);
@@ -58,7 +53,7 @@ class CrewUl extends React.Component {
       document.getElementById("crewAssembledPopUp").style.display = "block";
     });
     
-    this.changeCrew = this.changeCrew.bind(this);//Fancy js thing
+    this.changeCrew = this.changeCrew.bind(this);
   }
   removePlayer(somePlayer){
     this.setState({crew:this.state.crew.filter((x)=>(x!=somePlayer))});
@@ -128,6 +123,50 @@ function startGame(){
   };
 };
 
+class Stage1 extends React.Component {
+  constructor() {
+    super();
+    this.state = {waitingFor: globalCrew, ready: []};
+    
+    this.tooSlow = this.tooSlow.bind(this);
+  }
+  tooSlow(somePlayer){
+    if (globalCrewthis.state.ready.length >= 2){
+      this.setState({waitingFor:this.state.waitingFor.filter((x)=>(x!=somePlayer))});
+      globalCrew = globalCrew.filter((x)=>(x!=somePlayer));
+      socket.emit('too_slow', somePlayer);
+    } else {
+      hidePopUps();
+      document.getElementById("tooFewReady").style.display = "block";
+    };
+  }
+  render() {
+    return <div>
+      <div style={{backgroundColor: 'lightblue'}}>
+        <h2>Fill in your boards</h2>
+      </div>
+      <button>Done</button>
+      <h2>Waiting for:</h2>
+      <ul>
+        {this.state.waitingFor.map(crewMember => (
+          <li style={{position:'relative'}}>
+            <div className="cross" onClick={() => this.tooSlow(crewMember)}>&times;</div>
+            <div className="nameLiDiv">{crewMember}</div>
+          </li>
+        ))}
+      </ul>
+      <h2>Ready:</h2>
+      <ul>
+        {this.state.ready.map(crewMember => (
+          <li style={{position:'relative'}}>
+            <div className="nameLiDiv">{crewMember}</div>
+          </li>
+        ))}
+      </ul>
+    </div>;
+  }
+};
+
 var toRender = <div>
     <div className="stage0">
       <div style={{position: 'relative', minHeight: 'calc(100vh - 230px)'}}>
@@ -139,28 +178,32 @@ var toRender = <div>
         </div>
         <h2 style={{fontSize: '50px', margin: '0px', marginLeft: '10px'}}>Crew:</h2>
         <CrewUl />
-        <div id="popUps">
-          <div id="waiting" className="popUp"><div>
-              <h3>Waiting</h3>
-              <hr />
-              <p>This won&apos;t take too long, I hope!</p>
-           </div></div>
-        <div id="tooFew" className="popUp"><div>
-            <h3>Too Few Crewmembers</h3>
-            <hr />
-            <p>Yarr, ye be needin&apos; at least 2 players.</p>
-            <button className="close" onClick={hidePopUps}>Okay!</button>
-        </div></div>
       </div> 
     </div>
   </div>
   <div className="stage1">
-    <h4>Hey</h4>
+    <Stage1 />
   </div>
+  <div id="popUps">
+    <div id="waiting" className="popUp"><div>
+        <h3>Waiting</h3>
+        <hr />
+        <p>This won&apos;t take too long, I hope!</p>
+    </div></div>
+    <div id="tooFew" className="popUp"><div>
+        <h3>Too Few Crewmembers</h3>
+        <hr />
+        <p>Yarr, ye be needin&apos; at least 2 players.</p>
+        <button className="close" onClick={hidePopUps}>Okay!</button>
+    </div></div>
+    <div id="tooFewReady" className="popUp"><div>
+        <h3>Too Few Crewmembers Ready</h3>
+        <hr />
+        <p>Yarr, ye be needin&apos; at least 2 ready players before ye can drop people for bein&apos; slow.</p>
+        <button className="close" onClick={hidePopUps}>Okay!</button>
+    </div></div>
 </div>;
 
 ReactDOM.render(toRender, root);
-
-//var popUps = document.getElementsByClassName("popUp");  
 
 socket.emit('request_key');
