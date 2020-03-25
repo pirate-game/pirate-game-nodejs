0c73b851f7d12a575547d3a1b1d17966cc2272cd
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-25 10:34:34 +0000

    Update Watch.js

--- public/Watch.js
+++ public/Watch.js
@@ -2,4 +2,4 @@ var a = new URLSearchParams(window.location.search);
 
 var toRender = <img src={"imgs/"+a.get("which")} />;
 
-ReactDOM.render(toRender, document.getElementById('root'););
+ReactDOM.render(toRender, document.getElementById('root'));
