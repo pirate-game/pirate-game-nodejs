2ec10ee42eb7a98fc07249015442e6ab88f2d852
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-30 18:10:39 +0100

    Update Join.js

--- public/Join.js
+++ public/Join.js
@@ -247,7 +247,7 @@ function Place(props){
       <hr />
       <p>In what grid square would you like to place The &apos;{props.which[0].toUpperCase()+props.which.substr(1)}&apos; Symbol?<br />You can click on the square to select it.</p>
       <input type="text" id={"placeInput"+props.which} maxLength="2" />
-      <button className="choosePlace" onClick={() => attemptPlace(props.which)} style={{height:"unset",display:"block",marginTop:"10px"}}>Okay!</button>
+      <button className="choosePlace close" onClick={() => attemptPlace(props.which)} style={{height:"unset",display:"block",marginTop:"10px"}}>Okay!</button>
     </div>);
 };
       
