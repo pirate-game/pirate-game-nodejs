8dc11d5a87bfba844eba09a00b7c653d75f0981a
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-03-31 18:51:57 +0100

    Update Join.js

--- public/Join.js
+++ public/Join.js
@@ -327,10 +327,8 @@ function attemptPlace3000(){
         hidePopUps();
         document.getElementById("squareTaken").style.display = "block";
       } else {
-        console.log(proposedSquares[0]);
         theBoard.updateBoard(proposedSquares[0], "3000");
-        console.log(proposedSquares[1]);
-        theBoard.updateBoard(proposedSquares[1], "3000");
+        theBoard.updateBoard(proposedSquares[1], "3000");//Only does the last one. WHY!?
         document.getElementById("place3000").style.display = "none";
         document.getElementById("place1000").style.display = "block";
       };
