7241c162e8608ba1b0ca0efbbb417b728a8747aa
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-04-03 14:49:59 +0100

    Update gamethings.js

--- public/gamethings.js
+++ public/gamethings.js
@@ -1,2 +1,25 @@
 function unloadFn(event){event.returnValue="";};
-window.addEventListener("beforeunload",unloadFn);
+window.addEventListener("beforeunload",unloadFn);];
+
+socket.on('debugmsg', function(msg){console.log(msg)});
+
+function showStage(stage){
+  var stageElements = document.getElementsByClassName(stage);
+  for (var i = 0; i < stageElements.length; i++){
+    stageElements[i].style.display = "block";
+  };
+};
+
+function hideStage(stage){
+  var stageElements = document.getElementsByClassName(stage);
+  for (var i = 0; i < stageElements.length; i++){
+    stageElements[i].style.display = "none";
+  };
+};
+
+function hidePopUps(){
+  var popUps = document.getElementsByClassName("popUp");
+  for (var i = 0; i < popUps.length; i++){
+    popUps[i].style.display = "none";
+  };
+};
