994cd9616d075deeb3095d73cdc6254f63261507
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-04-07 09:21:33 +0100

    Update Start.js

--- public/Start.js
+++ public/Start.js
@@ -355,8 +355,9 @@ function nextSquareMid(){
   };
   globalCrew = globalCrew.filter(e=>!unreadyCrew.includes(e));
   unreadyCrew = globalCrew;
+  console.log(theBoard.state.done.length);
   if (theBoard.state.done.length == 49){
-    document.getElementById("nextSquare").style.display = "none";/*Make These Work*/
+    document.getElementById("nextSquare").style.display = "none";
     document.getElementById("showScores").style.display = "block";
   };
 };
