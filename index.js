16c9f401e70d8f337eb11f8f40b413ffacb5b4cd
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-04-08 09:34:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ io.on('connection', function(socket){
         var player = gameAndNameToPlayer(games[pos], who[i]);
         if (player != {}){
           player.emit('too_slow');
-          games[pos].crew = games[pos].crew.filter((x)=>(x.id!=player.id));
+          games[pos].crew = games[pos].crew.filter((x)=>(x.pirateName!=who[i]));
         };
       };
       var thoseWatching = games[pos].watching;
