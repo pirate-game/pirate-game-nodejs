afd9336e675339a689c8dd9aafffb1c2f78df9f5
The Pirate King <60094408+pirate-game@users.noreply.github.com>
2020-08-28 16:34:32 +0100

    Update navbar.js
    Pre-compiled navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -1,11 +1,20 @@
-const navbar = <div>
-	    		<ul>
-				<li><a id="logo" href="index.html"><img border="0" src="logo.png" /></a></li>
-				<li><a href="Start.html">Start&nbsp;a<br />Game</a></li>
-				<li><a href="Join.html">Join&nbsp;a<br />Game</a></li>
-				<li><a href="Watch.html">Watch&nbsp;a<br />Game</a></li>
-				<li><a href="Acknowledge.html">Acknowledgements</a></li>
-	    		</ul>
-			<h1 id="title">&nbsp;The&nbsp;Pirate&nbsp;Game&nbsp;</h1>
-		</div>;
+"use strict";
+
+var navbar = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
+  id: "logo",
+  href: "index.html"
+}, /*#__PURE__*/React.createElement("img", {
+  border: "0",
+  src: "logo.png"
+}))), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
+  href: "Start.html"
+}, "Start\xA0a", /*#__PURE__*/React.createElement("br", null), "Game")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
+  href: "Join.html"
+}, "Join\xA0a", /*#__PURE__*/React.createElement("br", null), "Game")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
+  href: "Watch.html"
+}, "Watch\xA0a", /*#__PURE__*/React.createElement("br", null), "Game")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
+  href: "Acknowledge.html"
+}, "Acknowledgements"))), /*#__PURE__*/React.createElement("h1", {
+  id: "title"
+}, "\xA0The\xA0Pirate\xA0Game\xA0"));
 ReactDOM.render(navbar, document.getElementById("nav"));
